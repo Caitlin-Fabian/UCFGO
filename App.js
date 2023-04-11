673fd9f843076bb06b16bf613fb6c664bf998732
Jorge Riera <jorgelriera@gmail.com>
2023-04-11 14:02:17 -0400

    Squashed commit of the following:
    commit e8de05cbc477aadf0f8a1948a975bafda6393ad3
    Author: Jorge Riera <joluriera@gmail.com>
    Date:   Mon Apr 10 15:43:47 2023 -0400
    
        Updated map screen to master version
    
    commit b60fdda97c74c65aaf07a8d80cba71d37e240102
    Author: Jorge Riera <joluriera@gmail.com>
    Date:   Mon Apr 10 15:11:02 2023 -0400
    
        Page structure for login
    
    commit e55621798f0e026cd265d01bfca5f55ccad1106c
    Author: Jorge Riera <joluriera@gmail.com>
    Date:   Fri Apr 7 15:39:21 2023 -0400
    
        Fixed login keyboard issue
    
    commit 743fa3c1546c3af2e6dbb7aa903b8197af3906de
    Author: Jorge Riera <joluriera@gmail.com>
    Date:   Fri Apr 7 15:32:38 2023 -0400
    
        Form selector button/Log in functional
    
    commit 5d30af3828dbc636dcfc16fa2830f262376850ee
    Author: Jorge Riera <joluriera@gmail.com>
    Date:   Mon Apr 3 14:52:26 2023 -0400
    
        React Navigation base
    
    commit 3ae669d95bcdd6dce6c7494f156064830c2da0da
    Author: Jorge Riera <joluriera@gmail.com>
    Date:   Tue Mar 28 19:29:42 2023 -0400
    
        Test initial commit

--- App.js
+++ App.js
@@ -1,265 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { LogBox } from 'react-native';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
-import ActionButton from 'react-native-action-button';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-import {mapStyle} from "./mapStyle";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginPage from "./pages/LoginPage.js";
+import MapScreen from "./pages/MapScreen.js";
+import EmailVerification from "./pages/EmailVerification.js";
+import ForgotPassword from "./pages/ForgotPassword.js"
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-    useEffect(() => {
-        LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
-    });
-
-    const [shouldShowButtons, setShouldShowButtons] = useState(false);
-    const [shouldShowProfile, setShouldShowProfile] = useState(false);
-    const [shouldShowInventory, setShouldShowInventory] = useState(false);
-    const [shouldShowSettings, setShouldShowSettings] = useState(false);
-    const [shouldBack, setShouldBack] = useState(false);
-    const [currLocation, setCurrLocation] = useState({coords: {latitude: 28.60160681694149, longitude: -81.20044675481425}});
-
-    function canInteract(coords) {
-        const distance = Math.sqrt(Math.pow(coords.latitude - currLocation.coords.latitude, 2) + Math.pow(coords.longitude - currLocation.coords.longitude, 2));
-        if (distance <= 0.0008)
-            return true;
-        else
-            return false;
-    }
-
-    useEffect(() => {
-        (async () => {
-          
-          let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return;
-          }
-    
-          let location = await Location.getCurrentPositionAsync({});
-          setCurrLocation(location);
-          //console.log(location)
-        })();
-      }, []);
-
     return (
-        <View style={styles.container}>
-            <MapView
-                provider='google' 
-                style={styles.map}
-                initialRegion={{
-                    latitude: 28.60160681694149,
-                    longitude: -81.20044675481425,
-                    latitudeDelta: 0.0200,
-                    longitudeDelta: 0.0005 ,
-                  }}
-                showsUserLocation={true}
-                followsUserLocation={true}
-                customMapStyle={mapStyle}
-            >
-                <Marker coordinate={{latitude: currLocation.coords.latitude, longitude: currLocation.coords.longitude}} onPress={e => canInteract(e.nativeEvent.coordinate) ? console.log("close enough") : console.log("not close enough")}/>
-                <Marker coordinate={{latitude: 28.60160681694149, longitude: -81.20044675481425,}} onPress={e => canInteract(e.nativeEvent.coordinate) ? console.log("close enough") : console.log("not close enough")}/> 
-            </MapView>
-            <Image style={styles.logoContainer} source={require('./Logo.png') }/>
-            <ActionButton autoInactive='true'>
-                <ActionButton.Item>
-                    <Button title='profile' titleStyle onPress={() => {setShouldShowProfile(!shouldShowProfile); setShouldShowButtons(!shouldShowButtons);}} />
-                </ActionButton.Item>
-                <ActionButton.Item>
-                    <Button title='inventory' titleStyle onPress={() => {setShouldShowInventory(!shouldShowInventory); setShouldShowButtons(!shouldShowButtons);}}/>                    
-                </ActionButton.Item>
-                <ActionButton.Item>
-                    <Button title='settings' titleStyle onPress={() => {setShouldShowSettings(!shouldShowSettings); setShouldShowButtons(!shouldShowButtons);}}/>
-                </ActionButton.Item>
-            </ActionButton>
-            {shouldShowProfile ? 
-                <View style={styles.greyOverlay}>
-                    <View style={styles.headerContainer}>
-                        <Image style={styles.logoContainer} source={require('./Logo.png')} />
-                        <TouchableOpacity style={styles.backButtonContainer} activeOpacity={0.2} onPress={() => setShouldShowProfile(!shouldShowProfile)}>
-                            <Image style={styles.backButton} source={require('./BackButton.png')} />
-                        </TouchableOpacity>
-                        <Text style={styles.titleTxt}>PROFILE</Text>
-                        <View style={styles.profilePicContainer}></View>
-                        <View style={styles.profileInfoContainter}></View>
-                    </View>
-                </View>
-            : null}
-            {shouldShowInventory ? 
-                <View style={styles.greyOverlay}>
-                    <View style={styles.headerContainer}>
-                        <Image style={styles.logoContainer} source={require('./Logo.png')} />
-                        <TouchableOpacity style={styles.backButtonContainer} activeOpacity={0.2} onPress={() => setShouldShowInventory(!shouldShowInventory)}>
-                            <Image style={styles.backButton} source={require('./BackButton.png')} />
-                        </TouchableOpacity>
-                        <Text style={styles.titleTxt}>MONSTERS</Text>
-                    </View>
-                    <View style={styles.monsterCardsContainer}>
-                        <View style={styles.monsterCard}></View>
-                        <View style={styles.monsterCard}></View>
-                        <View style={styles.monsterCard}></View>
-                    </View>
-                </View>
-            : null}
-            {shouldShowSettings ? 
-                <View style={styles.greyOverlay}>
-                    <View style={styles.headerContainer}>
-                        <Image style={styles.logoContainer} source={require('./Logo.png')} />
-                        <TouchableOpacity style={styles.backButtonContainer} activeOpacity={0.2} onPress={() => setShouldShowSettings(!shouldShowSettings)}>
-                            <Image style={styles.backButton} source={require('./BackButton.png')} />
-                        </TouchableOpacity>
-                        <Text style={styles.titleTxt}>SETTINGS</Text>
-                    </View>
-                    <View style={styles.changePasswordContainer}><Text style={{fontWeight: 'bold', fontSize: 20}}>CHANGE PASSWORD</Text></View>
-                    <View style={styles.signOutContainer}><Text style={{fontWeight: 'bold', fontSize: 20}}>SIGN OUT</Text></View>
-                </View>
-            : null}
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Login"
+                    component={LoginPage}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen
+                    name="Email"
+                    component={EmailVerification}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen
+                    name="Password"
+                    component={ForgotPassword}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen
+                    name="Map"
+                    component={MapScreen}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#bebebe',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logoContainer: {
-        position: 'absolute',
-        alignSelf: 'center',
-        top: 50,
-        height: 50,
-        width: 50,
-    },
-    buttonsContainer: {
-        position: 'absolute',
-        bottom: 0,
-        right: 0
-    },
-    profileButton: {
-        height: 90,
-        width: 90,
-        position: 'absolute',
-        backgroundColor: '#d4af37',
-        borderRadius: 100,
-        bottom: 140,
-        right: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inventoryButton: {
-        height: 90,
-        width: 90,
-        position: 'absolute',
-        backgroundColor: '#d4af37',
-        borderRadius: 100,
-        bottom: 250,
-        right: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    settingsButton: {
-        height: 90,
-        width: 90,
-        position: 'absolute',
-        backgroundColor: '#d4af37',
-        borderRadius: 100,
-        bottom: 360,
-        right: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    greyOverlay: {
-        position: 'absolute',
-        backgroundColor: 'rgba(70, 70, 70, 0.8)',
-        height: '100%',
-        width: '100%'
-    },
-    backButtonContainer: {
-        position: 'absolute',
-        height: '5%',
-        height: '5%',
-        top: 65,
-        left: 25
-    },
-    backButton: {
-        height: 35,
-        width: 35
-    },
-    headerContainer: {
-        position: 'absolute',
-        top: 0,
-        width: '100%',
-        height: 200,
-        backgroundColor: '#ffc700',
-    },
-    titleTxt: {
-        position: 'absolute',
-        top: 130,
-        alignSelf: 'center',
-        fontWeight: 'bold',
-        fontSize: 40
-    },
-    profilePicContainer: {
-        position: 'absolute',
-        top: 220,
-        alignSelf: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 25,
-        height: 250,
-        width: 250
-    },
-    profileInfoContainter: {
-        position: 'absolute',
-        top: 500,
-        alignSelf: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 25,
-        height: 300,
-        width: 350
-    },
-    monsterCardsContainer: {
-        position: 'absolute',
-        top: 220,
-        alignSelf: 'center',
-        height: 570,
-        width: '100%',
-        alignItems: 'flex-start',
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
-    monsterCard: {
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        height: 100,
-        width: 100
-    },
-    changePasswordContainer: {
-        position: 'absolute',
-        bottom: 30,
-        alignSelf: 'center',
-        width: '80%',
-        height: '7%',
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    signOutContainer: {
-        position: 'absolute',
-        bottom: 115,
-        alignSelf: 'center',
-        width: '80%',
-        height: '7%',
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    map: {
-        width: '100%',
-        height: '100%'
-    }
-});
+};
