51c917c24ac207735d4ff35f28d049b6aac4a258
Caitlin <caitlinfstar@gmail.com>
2023-04-12 23:40:14 -0400

    Implementing JWT Token decoding

---
+++ tokenStorage.js
@@ -0,0 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+exports.storeToken = function (tok) {
+    try {
+        AsyncStorage.setItem('token_data', tok.accessToken);
+        console.log('Access token Hello: **** ' + tok.accessToken);
+        // console.log(tok.accessToken);
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+exports.retrieveToken = async () => {
+    var ud;
+    try {
+        console.log('BOo');
+        const data = await AsyncStorage.getItem('token_data');
+        if (data !== null) {
+            console.log(data);
+            return data;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
